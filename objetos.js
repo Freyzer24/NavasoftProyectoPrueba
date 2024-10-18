2ad78b7e9c6e2a64f7489633588e679329c20e3f
carlossaul23 <cvasquezquiros@gmail.com>
2024-10-18 10:08:00 -0600

    delete objetos.js

--- objetos.js
+++
@@ -1,9 +0,0 @@
-// Función constructora para crear usuarios
-function Usuario(nombre, telefono, email, usuario, rol, contraseña) {
-    this.nombre = nombre;
-    this.telefono = telefono;
-    this.email = email;
-    this.usuario = usuario;
-    this.rol = rol;
-    this.contraseña = contraseña;
-  }
